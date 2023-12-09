150d6c5d60629de860d99fc0689699bcf3ce160f
babu97 <benkaimugul@gmail.com>
2023-12-09 01:04:56 +0000

    finished assigments

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Bear1', 10, 'M', 'Brown', 'Friendly', 1),
+    ('Bear2', 8, 'F', 'Black', 'Energetic', 1),
+    ('Bear3', 15, 'M', 'Gray', 'Calm', 1),
+    ('Bear4', 5, 'F', 'White', 'Playful', 1),
+    ('Bear5', 12, 'M', 'Brown', 'Aggressive', 1),
+    ('Bear6', 7, 'F', 'Black', 'Shy', 1),
+    ('Bear7', 9, 'M', 'Brown', 'Friendly', 1),
+    (NULL, 11, 'F', 'Gray', 'Calm', 1);
